205e56ce92c5377e84b066203ea99cb221c24087
Emanuel Jaime <emanuel.jaime@solera.com>
2023-03-15 13:58:20 +0100

    Final with Images

--- src/Section.jsx
+++ src/Section.jsx
@@ -1,14 +1,21 @@
-import {useState} from 'react';
+import {useState, useRef} from 'react';
 import DefaultImg from '../resources/img.jpg';
 import './style.css'
 
 export const Bttn = ( {value}) =>{
-
+    
+    const ref = useRef()
     const [BaseImg, setBaseImg] = useState (DefaultImg);
+    
+    const handleClick = (e) => {
+        ref.current.click()
+        setBaseImg( () => "../resources/Solera_Logo.png")
+    }
+
     const handleBaseImg = () => {
         setBaseImg( () => "../resources/Solera_Logo.png")
     }
-    
+
     
     return(
         <div className='center'>
@@ -16,7 +23,9 @@ export const Bttn = ( {value}) =>{
                 <div>
                     <img src={BaseImg}/>
                 </div>
+                <input ref={ref} type="file" />
                 <button onClick={ handleBaseImg }> Upload Base Image </button>
+                
                 <p>Estamos intentando  izquierda</p>
             </div> 
             
